2d2f08f6fa9611a69a9c73a586cb5aa9645da39e
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-12-05 20:52:36 +0700

    created Halaman Entry file and DestinasiEntry object

---
+++ app/src/main/java/com/example/meet10pam/ui/HalamanEntry.kt
@@ -0,0 +1,10 @@
+package com.example.meet10pam.ui
+
+import com.example.meet10pam.R
+import com.example.meet10pam.navigasi.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+
