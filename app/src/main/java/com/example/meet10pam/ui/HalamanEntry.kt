1bdb3b7c6a28f82c0543249dc1cd0ae5eb234049
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-12-05 21:09:08 +0700

    added EntrySiswaBody function

--- app/src/main/java/com/example/meet10pam/ui/HalamanEntry.kt
+++ app/src/main/java/com/example/meet10pam/ui/HalamanEntry.kt
@@ -1,10 +1,51 @@
 package com.example.meet10pam.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meet10pam.R
+import com.example.meet10pam.model.DetailSiswa
+import com.example.meet10pam.model.UIStateSiswa
 import com.example.meet10pam.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.coroutineScope
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+            ) {
+            Text(stringResource(id = R.string.btn_submit))
+
+        }
+
+    }
+
 }
 
