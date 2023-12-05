82832540341455672a382dd7b58edc190b7d3cb7
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-12-05 19:51:21 +0700

    completed PenyediaViewModel

--- app/src/main/java/com/example/meet10pam/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/meet10pam/model/PenyediaViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.meet10pam.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.meet10pam.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory =  viewModelFactory {
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/*
+Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+[AplikasiSiswa]
+ */
+fun CreationExtras.AplikasiSiswa():AplikasiSiswa = (this
+        [ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
