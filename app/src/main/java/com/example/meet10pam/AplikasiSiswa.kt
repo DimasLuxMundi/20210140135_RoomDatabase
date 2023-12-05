dafcdda43681bda5df428db7f4946e6ed1706b08
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-12-05 19:44:15 +0700

    completed AplikasiSiswa

--- app/src/main/java/com/example/meet10pam/AplikasiSiswa.kt
+++ app/src/main/java/com/example/meet10pam/AplikasiSiswa.kt
@@ -1,4 +1,19 @@
 package com.example.meet10pam
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.meet10pam.repositori.ContainerApp
+import com.example.meet10pam.repositori.ContainerDataApp
+
+class AplikasiSiswa: Application() {
+    /*
+    AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+
+
 }
